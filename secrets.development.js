e005c4dfb3115aa1de505984137be493de1490d6
minglitchfe <ming@glitch.finance>
2022-03-15 10:20:40 +0700

    Update config ws provider

--- secrets.development.js
+++ secrets.development.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  wsProvider: 'ws://fullnodes-testnet-1.glitch.finance:9944',
+  wsProvider: 'wss://wss-fullnodes-testnet.glitch.finance',
   baseApiUrl: 'https://api-testnet-v2.glitch.finance',
   explorerUrl: 'https://testnet-explorer.glitch.finance',
   walletVersion: '1.0.0',
